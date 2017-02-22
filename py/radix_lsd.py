f6124b8deedd64d5d1ff0d29b2acc56b47c49e06
Kevin Morris <kevr.gtalk@gmail.com>
2017-02-21 17:06:46 -0800

    Fix Python radix_lsd bug
    Signed-off-by: Kevin Morris <kevr.gtalk@gmail.com>

--- py/radix_lsd.py
+++ py/radix_lsd.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-def _pass(x, place):
+def _pass(x, position):
     # Create 10 buckets for each digit
     bucket = [[] for e in range(10)]
 
+    # Divisor
+    div = int(pow(10, position - 1))
+
     for v in x:
-        idx = int(v / pow(10, place - 1) % 10)
+        idx = int(int(v / div) % 10)
         bucket[idx].append(v)
 
     i = 0
@@ -18,9 +21,12 @@ def radix_sort(x):
     N = len(str(max(x)))
     place = 1
 
-    while place < N:
+    while place <= N:
         _pass(x, place)
         place = place + 1
 
     return x
+
+L = [66, 33, 99, 666, 333, 431, 2346, 999]
+print(radix_sort(L))
 
